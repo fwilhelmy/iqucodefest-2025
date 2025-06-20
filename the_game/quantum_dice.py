474fc38264398dc922b462a003f29f0ddb335c04
Kuift Kotar <kuiftk@gmail.com>
2025-06-20 11:37:48 -0400

    transformed dice into quantum walk

--- the_game/quantum_dice.py
+++ the_game/quantum_dice.py
@@ -3,20 +3,33 @@ from qiskit_aer import AerSimulator
 
 _simulator = AerSimulator()
 
-def quantum_walk_roll():
-    """Return a dice roll using a simple quantum walk.
+def controlled_displacement(qc):
+        qc.mcx([0,1,2], 3, ctrl_state=None, mode='noancilla')
+        qc.mcx([0,1], 2, ctrl_state=None, mode='noancilla')
+        qc.mcx([0,], 1, ctrl_state=None, mode='noancilla')
+        return qc
 
-    The circuit creates a uniform superposition over 3 qubits and measures it.
-    Results 0-5 are mapped to dice values 1-6. Values 6 or 7 are retried.
+def quantum_walk_roll(step=6):
+    """Return a dice roll using a quantum walk.
     """
-    while True:
-        qc = QuantumCircuit(3, 3)
-        qc.h([0, 1, 2])
-        qc.measure([0, 1, 2], [0, 1, 2])
-        result = _simulator.run(qc, shots=1).result()
+    get_out = 0
+    while get_out <= 1000:
+        qc = QuantumCircuit(4, 3)
+        qc.x(0)
+        qc.h(0)
+        random_walk_nb_steps = step
+        for i in range(random_walk_nb_steps):
+            qc = controlled_displacement(qc)
+            qc.h(0)
+        qc.measure([1, 2, 3], [0, 1, 2])
+        qc.draw(output='mpl', filename='quantum_walk_roll.png')
+        result = _simulator.run(qc, shots=10000).result()
         counts = result.get_counts()
         outcome = list(counts.keys())[0]
         value = int(outcome, 2)
-        if value < 6:
+
+
+        get_out += 1
+        if value < 6: # s'assure qu'on reste entre 0 et 6
             return value + 1
 
