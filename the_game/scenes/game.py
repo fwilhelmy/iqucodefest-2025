dbcbc40b6b29c159af9eb630446f80d51d2b7702
Kuift Kotar <kuiftk@gmail.com>
2025-06-20 07:18:14 -0400

    rebase 2

--- the_game/scenes/game.py
+++ the_game/scenes/game.py
@@ -122,7 +122,7 @@ class GameScene(Scene):
 
     def handle_event(self, e):
         if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
-            from scenes.menu import MenuScene
+            from the_game.scenes.menu import MenuScene
             self.manager.go_to(MenuScene(self.manager))
         elif self.moving_player is None:
             roll_keys = (pygame.K_SPACE, pygame.K_RETURN, pygame.K_r)
@@ -137,19 +137,15 @@ class GameScene(Scene):
                 self.move_timer = self.MOVE_DELAY
                 if self.steps_remaining <= 0:
                     self._end_move()
-            return
-
-        if self.moving_player is None:
-            if e.type == pygame.KEYDOWN and e.key in (pygame.K_SPACE, pygame.K_RETURN):
-                self._roll_one_die()
-            if self.roll_button.handle_event(e):
-                self._roll_one_die()
-
-
-        if self.roll_button.handle_event(e):
-            self._roll_one_die()
-
-        if e.type == pygame.QUIT:
+            elif e.key == pygame.K_MINUS:
+                self.zoom = self._clamp_zoom(self.zoom - self.ZOOM_STEP)
+            elif e.key == pygame.K_EQUALS:
+                self.zoom = self._clamp_zoom(self.zoom + self.ZOOM_STEP)
+            elif e.key == pygame.K_LEFT:
+                self.branch_index = (self.branch_index - 1) % len(self.branch_options)
+            elif e.key == pygame.K_RIGHT:
+                self.branch_index = (self.branch_index + 1) % len(self.branch_options)
+        elif e.type == pygame.QUIT:
             pygame.quit(); sys.exit()
 
         
